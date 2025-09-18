2b48aadf8b7dff3de0840f73b76ca06363b49562
yang dong won <didehddnjs89@gmail.com>
2025-09-18 17:01:55 +0900

    refactory: module pakage 정리

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -10,13 +10,15 @@ repositories {
 }
 
 dependencies {
-    implementation(project(mapOf("path" to ":src:main:domain")))
+    implementation(project(mapOf("path" to ":domain")))
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    // Aop
-    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+
 }
 
 tasks.test {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,65 +1,32 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.5.5"
-	id("io.spring.dependency-management") version "1.1.7"
-}
-
-group = "project.price_it"
-version = "0.0.1-SNAPSHOT"
-description = "crowdSourcing_api"
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
+	id("io.spring.dependency-management") version "1.1.7" apply false
+	id("org.springframework.boot") version "3.3.4" apply false
+	id("java-library") apply false
 }
 
 allprojects {
+	group = "project.price_it"
+	version = "0.0.1-SNAPSHOT"
+
 	repositories {
 		mavenCentral()
 	}
 }
 
-springBoot {
-	mainClass = "project.price_it.api.ApiApplication"
-}
-
-
-
 subprojects {
 	apply(plugin = "java-library")
-	apply(plugin = "org.springframework.boot")
 	apply(plugin = "io.spring.dependency-management")
 
-	group = "project.graduation.crowd_sourcing"
-	version = "0.0.1-SNAPSHOT"
-
-	configurations {
-		compileOnly {
-			extendsFrom(configurations.annotationProcessor.get())
+	java {
+		toolchain {
+			languageVersion.set(JavaLanguageVersion.of(17))
 		}
 	}
 
 	dependencies {
-		implementation("org.springframework.boot:spring-boot-starter")
 		testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-		// Lombok
+		// Lombok (공통 적용)
 		compileOnly("org.projectlombok:lombok:1.18.32")
 		annotationProcessor("org.projectlombok:lombok:1.18.32")
 		testCompileOnly("org.projectlombok:lombok:1.18.32")
