1ffca6b16cc7f4c2777beee484e022e4eb432d0d
yang dong won <didehddnjs89@gmail.com>
2025-10-01 09:43:18 +0900

    feat/#4: point repisotry, point service 생성

---
+++ domain/src/main/java/project/price_it/service/PointService.java
@@ -0,0 +1,10 @@
+package project.price_it.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PointService {
+    private final PointService pointService;
+}
